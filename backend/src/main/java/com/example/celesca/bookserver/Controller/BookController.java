0bc3ad89469035f8614f394e26ec18465b9ee396
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-22 23:25:32 +0700

    add: BookService

--- backend/src/main/java/com/example/celesca/bookserver/Controller/BookController.java
+++ backend/src/main/java/com/example/celesca/bookserver/Controller/BookController.java
@@ -1,6 +1,7 @@
 package com.example.celesca.bookserver.Controller;
 
 import com.example.celesca.bookserver.Model.Book;
+import com.example.celesca.bookserver.Service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,19 @@ import java.util.List;
 @RestController
 public class BookController {
 
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     private final List<Book> books = List.of(
         new Book("The Great Gatsby", "F. Scott Fitzgerald", "https://images-na.ssl-images-amazon.com/images/I/51Z6Zzq9OEL._SX331_BO1,204,203,200_.jpg")
     );
 
     @GetMapping("/api/books")
     public List<Book> getAllBooks() {
-        for (Book book : books) {
-            return books;
-        }
+        String result = bookService.GetBooks();
+        return result;
     }
 }
---
+++ backend/src/main/java/com/example/celesca/bookserver/Service/BookService.java
@@ -0,0 +1,11 @@
+package com.example.celesca.bookserver.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    public String GetBooks() {
+        return "Books";
+    }
+}
