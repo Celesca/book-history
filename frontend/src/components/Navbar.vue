b09c33130bd9990d437f17030cadadd81d03bc3b
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-06-28 21:14:14 +0700

    change to vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        
+    </nav>
+</template>
+
+<script setup>
+    name: 'Navbar',
+    data() {
+        return {
+            show: false
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
